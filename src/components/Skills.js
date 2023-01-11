d1f5dd12f4e4e994b1e58cbc73633eaf014bac7b
wayungi <fwayungi@gmail.com>
2023-01-11 20:38:46 +0300

    ADD: ressponsiveness to experience section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,44 +11,43 @@ const Skills = () => (
 
     {/* skills container */}
     <div className="max-w-[1000px] mx-auto flex flex-col justify-center w-full h-full">
-      <div>
-        <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 py-4">Experience</p>
-        <p>I have worked with te following technologies</p>
+      <div className="text-center sm:text-left sm:ml-8 lg:ml-20 pb-2">
+        <p className="text-4xl font-bold inline text-gray-300">Experience</p>
+        {/* <p>I have worked with te following technologies</p> */}
       </div>
 
       <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-        {/* shadow-md shadow-[#040c16] hover:scale-110 duration-500" */}
         <div className="flex flex-col items-center">
-          <DiPostgresql className="w-3/5 h-4/5 text-[#00a7e5]" />
-          <p className="text-white font-bold">PostgreSQL</p>
+          <DiPostgresql className="w-2/5 h-2/5 text-[#00a7e5] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">PostgreSQL</p>
         </div>
         <div className="flex flex-col items-center">
-          <DiRuby className="w-3/5 h-4/5 text-[#cc0000]" />
-          <p className="text-white font-bold">Ruby</p>
+          <DiRuby className="w-2/5 h-2/5 text-[#cc0000] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">Ruby</p>
         </div>
         <div className="flex flex-col items-center">
-          <SiJavascript className="w-3/5 h-4/5 text-[#F0DB4F]" />
-          <p className="text-white font-bold">JavaScript</p>
+          <SiJavascript className="w-2/5 h-2/5 text-[#F0DB4F] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">JavaScript</p>
         </div>
         <div className="flex flex-col items-center">
-          <SiRubyonrails className="w-3/5 h-4/5 text-[#cc0000]" />
-          <p className="text-white font-bold">Ruby on Rails</p>
+          <SiRubyonrails className="w-2/5 h-2/5 text-[#cc0000] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">Ruby on Rails</p>
         </div>
         <div className="flex flex-col items-center">
-          <SiCss3 className="w-3/5 h-4/5 text-[#264de4]" />
-          <p className="text-white font-bold">CSS3</p>
+          <SiCss3 className="w-2/5 h-2/5 text-[#264de4] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">CSS3</p>
         </div>
         <div className="flex flex-col items-center">
-          <SiTailwindcss className="w-3/5 h-4/5 text-[#008DD5]" />
-          <p className="text-white font-bold">TailWind</p>
+          <SiTailwindcss className="w-2/5 h-2/5 text-[#008DD5] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">TailWind</p>
         </div>
         <div className="flex flex-col items-center">
-          <FaReact className="w-3/5 h-4/5 text-[#008DD5]" />
-          <p className="text-white font-bold">React</p>
+          <FaReact className="w-2/5 h-2/5 text-[#008DD5] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">React</p>
         </div>
         <div className="flex flex-col items-center">
-          <AiOutlineHtml5 className="w-3/5 h-4/5 text-[#e34c26]" />
-          <p className="text-white font-bold">HTML5</p>
+          <AiOutlineHtml5 className="w-2/5 h-2/5 text-[#e34c26] hover:scale-110 duration-500" />
+          <p className="text-white font-bold my-4">HTML5</p>
         </div>
       </div>
     </div>
